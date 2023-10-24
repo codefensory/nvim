bae8d54d9ed3452848642511b9c8e74b543300b4
Yeferson Mejias <yefer.el.teke@gmail.com>
2023-10-24 11:24:09 -0500

    feat: remove gopls

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -249,7 +249,6 @@ return {
         'tsserver',
         'eslint',
         'rust_analyzer',
-        'gopls',
         'lua_ls',
         'jsonls',
         'bashls',
@@ -280,7 +279,6 @@ return {
         servers = {
           ['lua_ls'] = { 'lua' },
           ['rust_analyzer'] = { 'rust' },
-          ['gopls'] = { 'go' },
           -- if you have a working setup with null-ls
           -- you can specify filetypes it can format.
           -- ['null-ls'] = { 'javascript', 'typescript' },
@@ -390,44 +388,6 @@ return {
             },
           },
         },
-      })
-
-      lspconfig.gopls.setup({
-        settings = {
-          gopls = {
-            gofumpt = true,
-            codelenses = {
-              gc_details = false,
-              generate = true,
-              regenerate_cgo = true,
-              run_govulncheck = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
-            },
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
-            analyses = {
-              fieldalignment = true,
-              nilness = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
-            },
-            usePlaceholders = true,
-            completeUnimported = true,
-            staticcheck = true,
-            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
-          }
-        }
       })
 
       lsp.setup()
